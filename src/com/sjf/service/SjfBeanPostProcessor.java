118c13804e30d8d6a931608d9ec38ca3c93f9cef
shijunfeng <shijunfeng@thpower.com>
2025-07-03 06:26:51 +0800

    commit

---
+++ src/com/sjf/service/SjfBeanPostProcessor.java
@@ -0,0 +1,2 @@
+package com.sjf.service;public class SjfBeanPostProcessor {
+}
---
+++ src/com/sjf/spring/InitializingBean.java
@@ -0,0 +1,10 @@
+package com.sjf.spring;
+
+/**
+ * @author shijunfeng
+ */
+public interface BeanNameAware {
+
+    void setBeanName(String beanName);
+
+}
